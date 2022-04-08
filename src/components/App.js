c581019ecafa2b524b9231ae8c29885002728f78
alex <alexander_corral_valdez@outlook.com>
2022-04-08 17:36:34 -0500

    completoado

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import logo from '../logo.svg';
+import Game from './Game/Game';
+import './App.css';
+import abajo from './abajo.gif';
+import arriba from './arriba.gif';
+import acertado from './acertado.gif';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Game />
+      </header>
+      
+      
+    </div>
+  );
+}
+
+export default App;
